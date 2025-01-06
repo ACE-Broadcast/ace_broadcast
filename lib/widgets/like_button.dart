9c83f80b54b9686facfbe1829cdc417770a0aa9b
Mudit200408 <muditpurohit2004.pm@gmail.com>
2025-01-06 15:15:28 +0530

    like: Use lottie animation for the like button and also add animation to counter

--- lib/widgets/like_button.dart
+++ lib/widgets/like_button.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:flutter/services.dart';
 
 class LikeButton extends StatefulWidget {
   final bool isLiked;
@@ -14,14 +16,82 @@ class LikeButton extends StatefulWidget {
   State<LikeButton> createState() => _LikeButtonState();
 }
 
-class _LikeButtonState extends State<LikeButton> {
+class _LikeButtonState extends State<LikeButton>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  bool _isInitialized = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeController();
+  }
+
+  void _initializeController() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    // Set initial value based on isLiked
+    if (widget.isLiked) {
+      _controller.value = 1.0;
+    } else {
+      _controller.value = 0.0;
+    }
+    _isInitialized = true;
+  }
+
+  @override
+  void didUpdateWidget(LikeButton oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (!_isInitialized) {
+      _initializeController();
+    }
+    if (widget.isLiked != oldWidget.isLiked) {
+      if (widget.isLiked) {
+        _controller.forward();
+      } else {
+        _controller.reverse();
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    if (_isInitialized) {
+      _controller.dispose();
+    }
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (!_isInitialized) {
+      return const SizedBox(
+        width: 24,
+        height: 24,
+      );
+    }
+
     return GestureDetector(
-      onTap: widget.onTap,
-      child: Icon(
-        widget.isLiked ? Icons.favorite : Icons.favorite_border,
-        color: widget.isLiked ? Colors.red : Colors.grey,
+      onTap: () {
+        HapticFeedback.lightImpact();
+        widget.onTap?.call();
+      },
+      child: SizedBox(
+        width: 32,
+        height: 24,
+        child: OverflowBox(
+          maxWidth: 140,
+          maxHeight: 122,
+          child: Lottie.asset(
+            'assets/lottie/like.json',
+            controller: _controller,
+            fit: BoxFit.cover,
+            alignment: Alignment.center,
+          ),
+        ),
       ),
     );
   }
